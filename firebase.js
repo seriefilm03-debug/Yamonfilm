ea8796e1dea6ac3d6a7c2eeb727d56a088a6bb30
seriefilm03-debug <seriefilm03@gmail.com>
2026-05-12 13:26:10 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { 
-getAuth,
-setPersistence,
-browserLocalPersistence
+getAuth, 
+setPersistence, 
+browserLocalPersistence 
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-apiKey: "AIzaSyAQnexePSWAJdgZc2WO2BFKL8Z_jnDKbqE",
+apiKey: "TA_API_KEY",
 authDomain: "yamonfilm-fead8.firebaseapp.com",
 projectId: "yamonfilm-fead8"
 };
@@ -18,5 +18,4 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
-/* 🔥 OBLIGATOIRE */
 setPersistence(auth, browserLocalPersistence);
